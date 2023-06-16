a6e71e710e530e3ff7106303a31cace1775f2ec6
Ravindu Senarathna <80905394+MrRevengerX@users.noreply.github.com>
2023-06-17 00:53:25 +0530

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   metadataBase: new URL("https://xtremeleague.revengerx.dev"),
