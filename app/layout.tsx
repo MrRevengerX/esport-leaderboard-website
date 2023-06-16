20de5c05fde02fe684a1206f33f36f6026b0c3ee
Ravindu Senarathna <80905394+MrRevengerX@users.noreply.github.com>
2023-06-16 23:05:36 +0530

    Coming soon page done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+  metadataBase: new URL("https://xtremeleague.revengerx.dev"),
   title: "Xtreme League",
   description: "Xtreme League CODM Tournament Series",
+  openGraph: {
+    images: "/thumbnail.png",
+  },
+  twitter: {
+    images: "/thumbnail.png",
+  },
 };
 
 export default function RootLayout({
@@ -15,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
