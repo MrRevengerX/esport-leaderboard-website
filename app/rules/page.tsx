979260873c2b9eec06a3ca24e97b952f3e71fa5e
Ravindu Senarathna <80905394+MrRevengerX@users.noreply.github.com>
2023-06-24 21:39:15 +0530

    Added rules and registration pages.

---
+++ app/rules/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Rules() {
+  useEffect(() => {
+    window.location.replace(
+      "https://drive.google.com/file/d/1EYDo_Kj6xLeAsQ6HScqLAvI0dyFgBoQg/view?usp=drive_link"
+    );
+  }, []);
+  return;
+}
