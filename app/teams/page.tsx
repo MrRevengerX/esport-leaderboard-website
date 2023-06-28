052d016a259cd5c09a0d2f9ee952c81afcf94198
Ravindu Senarathna <80905394+MrRevengerX@users.noreply.github.com>
2023-06-28 14:01:14 +0530

    Fixed metadata for the teams page

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -1,11 +1,18 @@
-import LBTabs from "../components/leaderboard-tabs";
+import { Metadata } from "next";
+import TeamLeaderboard from "./team-leaderboard";
+
+export const metadata: Metadata = {
+  title: "Teams",
+};
 
 export default function Teams() {
   return (
-    <div className="flex min-h-screen flex-col items-center gap-16 bg-gray-900 px-4 py-36">
-      <h1 className="text-center text-4xl font-semibold text-white md:text-5xl lg:text-6xl">
+    <div className="flex min-h-screen flex-col items-center gap-8 bg-gray-900 px-4 py-36 text-white">
+      <h1 className="text-center text-4xl font-semibold md:text-5xl lg:text-6xl">
         <span className="outline-text">Xtreme</span> Ranks
       </h1>
+      <h2 className="text-center text-3xl font-thin">Teams</h2>
+      <TeamLeaderboard />
     </div>
   );
 }
---
+++ app/teams/team-leaderboard.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import TeamLeaderboardCard from "../components/team-leaderboard-card";
+import { getTeams } from "@/api/api";
+
+export default function TeamLeaderboard() {
+  const teamData = useQuery({
+    queryKey: ["teams"],
+    queryFn: getTeams,
+  });
+  return (
+    <div className="leaderboard flex max-w-5xl flex-col items-center gap-y-3 ">
+      {teamData.data?.map((team: any) => (
+        <TeamLeaderboardCard
+          key={team._id}
+          placement={team.placement}
+          teamName={team.name}
+          clan={team.clan}
+          teamLogo={team.logo}
+          teamRank={team.rank}
+          teamPoints={team.points}
+          played={team.matchesPlayed}
+          wins={team.wins}
+          losses={team.losses}
+        />
+      ))}
+    </div>
+  );
+}
